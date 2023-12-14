6d33a7a5d1095af87298ddbf1cd91a49f1b3da82
Nick Sette <njs2394@gmail.com>
2023-12-14 16:24:28 -0500

    cartoons

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,28 @@
-def roll_call_dwarves():
-    pass
 
-def summon_captain_planet():
-    pass
+# import ipdb
 
-def long_planeteer_calls():
-    pass
 
-def find_the_cheese():
-    pass
+def roll_call_dwarves(dwarf):
+    for index, value in enumerate(dwarf):
+        print(f'{index + 1}. {value}')
+
+
+def summon_captain_planet(planeteer_calls):
+    converted_list = [f"{x.title()}!" for x in planeteer_calls]
+    return (converted_list)
+
+
+def long_planeteer_calls(short_words):
+    return any(len(call) > 4 for call in short_words)
+
+
+def find_the_cheese(ingredients):
+    cheeses = ["cheddar", "gouda", "camembert"]
+
+    for ingredient in ingredients:
+        if ingredient in cheeses:
+            return ingredient
+    return None
+
+
+# ipdb.set_trace()
